3673fd7881b0eef9cfc7ead6d85b8f51ef68e49b
Toleti Rajeev <toleti.rajeev@jungleegames.com>
2024-12-23 23:07:46 +0530

    step1 completed

--- json_parser_test.go
+++ json_parser_test.go
@@ -4,12 +4,27 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"unicode"
 )
 
 func panicIfErr(err error){
 	if err != nil{
 		panic(err)
 	}
+}
+
+func getAnswer(name string) string{
+	val := strings.Split(name, ".")[0]
+	i := len(val) - 1
+	for i>=0{
+		if unicode.IsDigit(rune(val[i])){
+			i-=1
+		}else{
+			break
+		}
+	}
+	val = val[:i+1]
+	return val
 }
 
 func TestJsonParser(t *testing.T){
@@ -23,7 +38,7 @@ func TestJsonParser(t *testing.T){
 			testFileName := testFile.Name()
 			testFilePath := "./tests/" + filePath.Name() + "/" + testFileName
 			result := jsonParser(testFilePath)
-			if result != strings.Split(testFileName, ".")[0]{
+			if result != getAnswer(testFileName){
 				t.Errorf("File - %v in folder - %v failed the test.", testFileName, filePathName)
 			}
 		}
